701ac6511e2e3de63d73c100c663fc479d748056
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2020-08-26 15:26:04 +0530

    V1

---
+++ build/precache-manifest.dfe4b729650e893d39adb1b85c397c31.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648d25c6da0dbe1a33794db87d1406c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630f4cc27503659b4d48",
+    "url": "/static/css/main.15cc4631.chunk.css"
+  },
+  {
+    "revision": "e457c9002b645fa8f3b5",
+    "url": "/static/js/2.07fd8499.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.07fd8499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630f4cc27503659b4d48",
+    "url": "/static/js/main.ce5a8e42.chunk.js"
+  },
+  {
+    "revision": "9ddee07492978b9734d6",
+    "url": "/static/js/runtime-main.ffb40742.js"
+  },
+  {
+    "revision": "414ba6aa96eac8ff46057d2258c54062",
+    "url": "/static/media/Hero.414ba6aa.jpeg"
+  },
+  {
+    "revision": "a208a6fd6c1aff839f75ca24e1c5c46f",
+    "url": "/static/media/Hero2.a208a6fd.jpeg"
+  },
+  {
+    "revision": "73dbf2750551e1024d48b8cf11ae327c",
+    "url": "/static/media/lolo.73dbf275.webp"
+  },
+  {
+    "revision": "110c3feb8c235118a303341ecd96cd01",
+    "url": "/static/media/staticmap.110c3feb.jpg"
+  }
+]);
